afeceea4bbef91f5c6b96b0308c88fac86852b46
GangstaWangstaPranksta <49811885+GangstaWangstaPranksta@users.noreply.github.com>
2023-06-26 18:02:44 -0400

    add reactive theme and some button functionality

--- src/routes/box/[slug]/+page.svelte
+++ src/routes/box/[slug]/+page.svelte
@@ -1,26 +1,33 @@
 <script>
-  import { goto } from '$app/navigation';
-	import 'carbon-components-svelte/css/g80.css';
+	import { goto } from '$app/navigation';
+	import 'carbon-components-svelte/css/all.css';
 	import { Theme, TextArea, Button, ButtonSet, Modal } from 'carbon-components-svelte';
 	import Save from 'carbon-icons-svelte/lib/Save.svelte';
 	/** @type {import('./$types').PageData} */
 	export let data;
-	let contents = '';
-  let modalOpen = false;
-  const openModal = ()=>{
-    modalOpen = true;
-  }
+  let contents = data.contents
+  let savedContents = data.contents
+	let modalOpen = false;
+	const openModal = () => {
+    if(savedContents != contents){
+		  modalOpen = true;
+    }else{
+      goto("/");
+    }
+	};
 </script>
+
 <svelte:head>
 	<title>Box {data.box}</title>
 </svelte:head>
+<div class="wrapper">
 
-<Theme
+  <Theme
 	theme="g80"
 	tokens={{
-		'interactive-01': '#996600',
-		'hover-primary': '#ab7a1a',
-		'active-primary': '#6e4c09'
+    'interactive-01': data.primary,
+		'hover-primary': data.hover,
+		'active-primary': data.active
 	}}
 />
 
@@ -30,21 +37,29 @@
 	placeholder="List box items seprated by a new line..."
 	bind:value={contents}
 />
-
 <ButtonSet>
 	<Button kind="secondary" on:click={openModal}>Cancel</Button>
 	<Button icon={Save}>Save</Button>
 </ButtonSet>
 
 <Modal
-  danger
-  bind:open={modalOpen}
-  modalHeading="Exit without saving?"
-  primaryButtonText="Exit"
-  secondaryButtonText="Go Back"
-  on:click:button--secondary={() => (modalOpen = false)}
-  on:click:button--primary={() => {goto("/")}}
-  
+danger
+bind:open={modalOpen}
+modalHeading="Exit without saving?"
+primaryButtonText="Exit"
+secondaryButtonText="Go Back"
+on:click:button--secondary={() => (modalOpen = false)}
+on:click:button--primary={() => {
+  goto('/');
+}}
 >
-  <p>You have unsaved data</p>
+<p>You have unsaved data</p>
 </Modal>
+
+</div>
+
+<style>
+  .wrapper{
+    margin: 1.5em;
+  }
+</style>
