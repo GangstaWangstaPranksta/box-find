b148349cfdc672cf7dd247812faf28bdb7c8e6db
GangstaWangstaPranksta <49811885+GangstaWangstaPranksta@users.noreply.github.com>
2023-06-26 18:01:41 -0400

    add time reactive styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,22 @@
 <script>
-	import 'carbon-components-svelte/css/g80.css';
-	import { Theme } from 'carbon-components-svelte';
+  import { goto } from '$app/navigation';
+	import 'carbon-components-svelte/css/all.css';
+	import { Theme, Button } from 'carbon-components-svelte';
+  /** @type {import('./$types').PageData} */
+	export let data;
 </script>
 
-<Theme theme="g80" />
+<Theme
+	theme="g80"
+	tokens={{
+		'interactive-01': data.primary,
+		'hover-primary': data.hover,
+		'active-primary': data.active
+	}}
+/>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<Button on:click={()=>{goto("/box/582734")}}>
+  Test Box Page
+</Button>
