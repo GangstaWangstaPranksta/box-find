b1da7e10e2d0294927c6b5f328cbabf5c8aff0ae
GangstaWangstaPranksta <49811885+GangstaWangstaPranksta@users.noreply.github.com>
2023-06-27 11:04:56 -0400

    create new box

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,25 @@
 <script>
   import { goto } from '$app/navigation';
 	import 'carbon-components-svelte/css/all.css';
-	import { Theme, Button } from 'carbon-components-svelte';
+	import { Theme, Button, Modal, TextInput } from 'carbon-components-svelte';
+  import Add from 'carbon-icons-svelte/lib/Add.svelte'
   /** @type {import('./$types').PageData} */
 	export let data;
+  let showModal = false;
+  let newBoxID = ""; 
+
+  const newBox = async (id) =>{
+    const res = await fetch('/api/newBox', {
+			method: 'POST',
+			body: JSON.stringify({ id }),
+			headers: {
+				'content-type': 'application/json'
+			}
+		});
+		if ((await res.json()) == `${id} box created`) {
+			goto(`/box/${id}`)
+		}
+  };
 </script>
 
 <Theme
@@ -20,3 +36,23 @@
 <Button on:click={()=>{goto("/box/582734")}}>
   Test Box Page
 </Button>
+
+<Modal
+bind:open={showModal}
+modalHeading="Create a new Box"
+primaryButtonText="Create Box"
+secondaryButtonText="Cancel"
+on:click:button--secondary={() => (showModal = false)}
+on:click:button--primary={() => {
+  newBox(newBoxID);
+}}
+>
+<TextInput
+    id="box-name"
+    labelText="Box ID"
+    placeholder="Enter box ID..."
+    bind:value={newBoxID}
+  />
+</Modal>
+
+<Button on:click={()=>{showModal = true}} iconDescription="Create a new Box" icon={Add} />
