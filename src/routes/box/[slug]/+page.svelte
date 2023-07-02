848311d0600b5381ceadca94587148f56b8fba07
GangstaWangstaPranksta <49811885+GangstaWangstaPranksta@users.noreply.github.com>
2023-07-02 01:01:05 -0400

    rmv button set, add del & edit modals & funt+fixes

--- src/routes/box/[slug]/+page.svelte
+++ src/routes/box/[slug]/+page.svelte
@@ -5,20 +5,26 @@
 		Theme,
 		TextArea,
 		Button,
-		ButtonSet,
 		Modal,
-		ToastNotification
+		ToastNotification,
+		TextInput
 	} from 'carbon-components-svelte';
 	import Save from 'carbon-icons-svelte/lib/Save.svelte';
+	import Exit from 'carbon-icons-svelte/lib/Exit.svelte';
+	import Edit from 'carbon-icons-svelte/lib/Edit.svelte';
+	import TrashCan from 'carbon-icons-svelte/lib/TrashCan.svelte';
 	/** @type {import('./$types').PageData} */
 	export let data;
 	let id = data.box;
 	let contents = data.contents;
-	let savedContents = data.contents;
+	let initContents = data.contents;
 	let cancelModalOpen = false;
+	let editModalOpen = false;
+	let editBoxName = id;
+	let deleteModalOpen = false;
 	let toasts = [];
 	const openCancelModal = () => {
-		if (savedContents != contents) {
+		if (initContents != contents) {
 			cancelModalOpen = true;
 		} else {
 			goto('/');
@@ -33,9 +39,36 @@
 			}
 		});
 		if ((await res.json()) == 'saved') {
+			initContents = contents;
 			toasts = [...toasts, ''];
 		}
 	};
+	const delBox = async () =>{
+    const res = await fetch('/api/deleteBox', {
+			method: 'POST',
+			body: JSON.stringify({ id }),
+			headers: {
+				'content-type': 'application/json'
+			}
+		});
+		if ((await res.json()) == `${id} box deleted`) {
+			goto(`/`)
+		}
+  };
+	const renameBox = async () =>{
+    const res = await fetch('/api/renameBox', {
+			method: 'POST',
+			body: JSON.stringify({ id, editBoxName }),
+			headers: {
+				'content-type': 'application/json'
+			}
+		});
+		if ((await res.json()) == `${id} box renamed to ${editBoxName}`) {
+			goto(`/box/${editBoxName}`)
+			id = editBoxName;
+			editModalOpen = false;
+		}
+  };
 </script>
 
 <svelte:head>
@@ -50,18 +83,59 @@
 			'active-primary': data.active
 		}}
 	/>
+	<div class="header">
+		<h1>Box: {data.box}</h1>
+		<div class="boxEditButtons">
+			<Button kind="tertiary" iconDescription="Edit" icon={Edit} on:click={()=>{editModalOpen = true;}}/>
+			<Button kind="danger-tertiary" iconDescription="Delete" icon={TrashCan} on:click={()=>{deleteModalOpen = true;}}/>
+		</div>
+	</div>
+	<div class="editables">
+		<TextArea
+			labelText="Box Contents"
+			placeholder="List box items seprated by a new line..."
+			bind:value={contents}
+		/>
 
-	<h1>Box: {data.box}</h1>
-	<TextArea
-		labelText="Box Contents"
-		placeholder="List box items seprated by a new line..."
-		bind:value={contents}
+		<div class="images">
+			<h1>wow images or smth</h1>
+		</div>
+	</div>
+
+	<Button icon={Exit} kind="secondary" on:click={openCancelModal}>Exit</Button>
+	<Button icon={Save} on:click={save}>Save</Button>
+
+	<Modal
+		danger
+		bind:open={deleteModalOpen}
+		modalHeading="Delete this box?"
+		primaryButtonText="Delete"
+		secondaryButtonText="Cancel"
+		on:click:button--secondary={() => (deleteModalOpen = false)}
+		on:click:button--primary={() => {
+			delBox();
+		}}
+	>
+		<p>All data associated with this box will be deleted. This can not be reversed</p>
+	</Modal>
+	<Modal
+		bind:open={editModalOpen}
+		modalHeading="New Box Name/ID"
+		primaryButtonText="Change Box Name"
+		secondaryButtonText="Cancel"
+		on:click:button--secondary={() => (editModalOpen = false)}
+		on:click:button--primary={() => {
+			renameBox();
+		}}
+	>
+	<p>Any unsaved changed will be discarded.</p>
+		<TextInput
+		id="box-name"
+		labelText="Box ID"
+		placeholder="Enter box ID..."
+		bind:value={editBoxName}
 	/>
-	<ButtonSet>
-		<Button kind="secondary" on:click={openCancelModal}>Cancel</Button>
-		<Button icon={Save} on:click={save}>Save</Button>
-	</ButtonSet>
-
+	</Modal>
 	<Modal
 		danger
 		bind:open={cancelModalOpen}
@@ -91,10 +165,36 @@
 	.wrapper {
 		margin: 1.5em;
 	}
+	.header {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+	}
+	.boxEditButtons {
+		flex: none;
+	}
 	.toasts {
 		position: fixed;
 		z-index: 1;
 		bottom: 15px;
 		right: 15px;
+	}
+	.editables {
+		display: flex;
+		flex-direction: row;
+	}
+	.editables > * {
+		flex: 1;
+	}
+	/* .editables *{
+		padding-inline: 3em;
+	} */
+	@media (max-width: 1024px) {
+		.wrapper {
+			margin: 1em;
+		}
+		.editables {
+			flex-direction: column;
+		}
 	}
 </style>
