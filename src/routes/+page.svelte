7b80870fcf38944b62132a991cfea4151bdfb859
GangstaWangstaPranksta <49811885+GangstaWangstaPranksta@users.noreply.github.com>
2023-07-02 00:59:11 -0400

    remove sveltekit skeleton text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -30,8 +30,8 @@
 		'active-primary': data.active
 	}}
 />
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!-- <h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> -->
 
 <Button on:click={()=>{goto("/box/582734")}}>
   Test Box Page
