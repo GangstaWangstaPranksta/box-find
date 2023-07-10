78276063ef1fbb00e446516f82733a967cdff588
GangstaWangstaPranksta <49811885+GangstaWangstaPranksta@users.noreply.github.com>
2023-07-10 01:25:14 -0400

    formatting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -59,8 +59,6 @@
 		'active-primary': data.active
 	}}
 />
-<!-- <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> -->
 
 <div class="wrapper">
 	<div class="searchBar">
