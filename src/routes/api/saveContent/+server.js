820463ef40a20f076ebca635373abe1c83bdbb04
GangstaWangstaPranksta <49811885+GangstaWangstaPranksta@users.noreply.github.com>
2023-07-09 23:32:00 -0400

    rename /save to /saveContent

--- src/routes/api/saveContent/+server.js
+++ src/routes/api/saveContent/+server.js
@@ -3,9 +3,9 @@ import { JsonDB, Config } from 'node-json-db';
 
 /** @type {import('./$types').RequestHandler} */
 export async function POST({ request }) {
-    let db = new JsonDB(new Config("boxDatabase", false, false, '/'))
+    let db = new JsonDB(new Config("boxContentsDB", false, false, '/'))
     const { id, contents } = await request.json();
-    await db.push(`/${id}/contents`, contents)
+    await db.push(`/${id}`, contents)
     db.save();
     return json( "saved" );
 }
