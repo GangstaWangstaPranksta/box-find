a7c44a93b60a1c375785d6489bdbd44e9e3bf490
GangstaWangstaPranksta <49811885+GangstaWangstaPranksta@users.noreply.github.com>
2023-07-10 01:21:55 -0400

    transfer search from search-test to main path

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,40 @@
 <script>
-  import { goto } from '$app/navigation';
+	import { goto } from '$app/navigation';
 	import 'carbon-components-svelte/css/all.css';
-	import { Theme, Button, Modal, TextInput, ToastNotification } from 'carbon-components-svelte';
-  import Add from 'carbon-icons-svelte/lib/Add.svelte'
-  /** @type {import('./$types').PageData} */
+	import { Theme, Button, Modal, TextInput, Search, ClickableTile } from 'carbon-components-svelte';
+	import Add from 'carbon-icons-svelte/lib/Add.svelte';
+	import * as JsSearch from 'js-search';
+	/** @type {import('./$types').PageData} */
 	export let data;
-  let showModal = false;
-  let newBoxID = ""; 
-  let toasts = [];
+	let showModal = false;
+	let newBoxID = '';
 
-  const newBox = async (id) =>{
-    const res = await fetch('/api/newBox', {
+	let searchQuery = '';
+
+	let searchDoc = Object.keys(data.contents).map((key) => ({
+		id: key,
+		content: data.contents[key]
+	}));
+
+	var search = new JsSearch.Search('id');
+	search.indexStrategy = new JsSearch.AllSubstringsIndexStrategy();
+	search.addIndex('id');
+	search.addIndex('content');
+
+	search.addDocuments(searchDoc);
+
+	let result;
+
+	$: {
+		if (searchQuery != '') {
+			result = search.search(searchQuery);
+		} else {
+			result = searchDoc;
+		}
+	}
+
+	const newBox = async (id) => {
+		const res = await fetch('/api/newBox', {
 			method: 'POST',
 			body: JSON.stringify({ id }),
 			headers: {
@@ -18,10 +42,14 @@
 			}
 		});
 		if ((await res.json()) == `${id} box created`) {
-			goto(`/box/${id}`)
+			goto(`/box/${id}`);
 		}
-  };
+	};
 </script>
+
+<svelte:head>
+	<title>Home | Box Find</title>
+</svelte:head>
 
 <Theme
 	theme="g80"
@@ -34,46 +62,58 @@
 <!-- <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> -->
 
-<Button on:click={()=>{goto("/box/582734")}}>
-  Test Box Page
-</Button>
+<div class="wrapper">
+	<div class="searchBar">
+		<Search bind:value={searchQuery} />
+		<span class="newBox">
+			<Button
+				on:click={() => {
+					showModal = true;
+				}}
+				iconDescription="Create a new Box"
+				icon={Add}
+			/>
+		</span>
+	</div>
 
-<Modal
-bind:open={showModal}
-modalHeading="Create a new Box"
-primaryButtonText="Create Box"
-secondaryButtonText="Cancel"
-on:click:button--secondary={() => (showModal = false)}
-on:click:button--primary={() => {
-  newBox(newBoxID);
-}}
->
-<TextInput
-    id="box-name"
-    labelText="Box ID"
-    placeholder="Enter box ID..."
-    bind:value={newBoxID}
-  />
-</Modal>
-
-<Button on:click={()=>{showModal = true}} iconDescription="Create a new Box" icon={Add} />
-
-<div class="toasts">
-	{#each toasts as toast}
-		<ToastNotification
-			kind="success"
-			title="Uploaded"
-			subtitle="Your images have been uploaded successfully."
-			caption={new Date().toLocaleString()}
-		/>
-	{/each}
+	{#if result.length > 0}
+		{#each result as item}
+			<ClickableTile href="/box/{item.id}" style="margin-bottom: 1em;">
+				<h2>{item.id}</h2>
+				{item.content.replace(/\n/g, ', ')}
+				{#each data.photos[item.id] as photo}
+					<img src={photo} alt="o" style="width: 10vw" />
+				{/each}
+			</ClickableTile>
+		{/each}
+	{:else}
+		<h3>No results found for "{searchQuery}"</h3>
+		<p>But here's a cookie! 🍪</p>
+	{/if}
 </div>
 
+<Modal
+	bind:open={showModal}
+	modalHeading="Create a new Box"
+	primaryButtonText="Create Box"
+	secondaryButtonText="Cancel"
+	on:click:button--secondary={() => (showModal = false)}
+	on:click:button--primary={() => {
+		newBox(newBoxID);
+	}}
+>
+	<TextInput id="box-name" labelText="Box ID" placeholder="Enter box ID..." bind:value={newBoxID} />
+</Modal>
+
 <style>
-	.toasts{
-		position: fixed;
-		z-index: 1;
-		bottom: 15px;
-		right: 15px;
+	.wrapper {
+		margin: 1.5em;
+	}
+	.searchBar {
+		display: flex;
+		margin-bottom: 1em;
+	}
+	.newBox {
+		margin-left: 1em;
 	}
 </style>
