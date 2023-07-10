943b5b4e92ce8be198993d13d4e2a13972baa130
GangstaWangstaPranksta <49811885+GangstaWangstaPranksta@users.noreply.github.com>
2023-07-09 23:29:49 -0400

    fetch database contents for main page

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,5 +1,8 @@
+import { JsonDB, Config } from 'node-json-db';
 /** @type {import('./$types').PageLoad} */
-export function load() {
+export async function load() {
+	let contentDB = new JsonDB(new Config("boxContentsDB", false, false, '/'))
+	let imagesDB = new JsonDB(new Config("boxImagesDB", false, false, '/'))
 	let currentTime = new Date().getHours();
 	let primary = currentTime>7&&currentTime<21 ? "#19a619" : "#cf7f23"
 	let hover = currentTime>7&&currentTime<21 ? "#2cb82c" : "#d18a38"
@@ -7,6 +10,8 @@ export function load() {
 	return {
 		primary: primary,
 		hover: hover,
-		active: active
+		active: active,
+		contents: await contentDB.getData(`/`),
+		photos: await imagesDB.getData(`/`)
 	};
 }
