03c08e852c114a747862e4d1204dc2b89265354d
GangstaWangstaPranksta <49811885+GangstaWangstaPranksta@users.noreply.github.com>
2023-07-15 20:40:26 -0400

    fix rawImages

--- src/routes/api/rawImages/+server.js
+++ src/routes/api/rawImages/+server.js
@@ -3,5 +3,5 @@ import { JsonDB, Config } from 'node-json-db';
 /** @type {import('./$types').RequestHandler} */
 export async function GET() {
 	let imagesDB = new JsonDB(new Config('boxImagesDB', false, false, '/'));
-	return json(await contentDB.getData(`/`));
+	return json(await imagesDB.getData(`/`));
 }
