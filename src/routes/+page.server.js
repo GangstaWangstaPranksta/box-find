256dfd3c13fd87382a9c060b3197fd4fedde524f
Jason <jason@jasonx.dev>
2024-06-16 03:28:54 -0400

    fix dumb db queries

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -20,7 +20,7 @@ const client = new MongoClient(uri, {
 /** @type {import('./$types').PageLoad} */
 export async function load({}) {
 	let lastPage;
-	let images, contents;
+	let contents;
 	try {
 		// Connect the client to the server	(optional starting in v4.7)
 		await client.connect();
@@ -28,14 +28,10 @@ export async function load({}) {
 
 		lastPage = Math.ceil((await collection.countDocuments()) / 10);
 
-		let imagesCursor = collection
-			.find({}, { sort: { lastModified: -1 }, projection: { images: 1 } })
-			.limit(10);
 		let contentsCursor = collection
-			.find({}, { sort: { lastModified: -1 }, projection: { contents: 1 } })
+			.find({}, { sort: { lastModified: -1 }, projection: { images: 1, contents:1 } })
 			.limit(10);
 
-		images = await imagesCursor.toArray();
 		contents = await contentsCursor.toArray();
 	} finally {
 		// Ensures that the client will close when you finish/error
@@ -43,31 +39,7 @@ export async function load({}) {
 	}
 
 	return {
-		contents: transformContents(contents),
-		photos: transformImgs(images),
+		contents: contents,
 		lastPage: lastPage
 	};
-}
-
-function transformImgs(arr) {
-	const result = {};
-
-	arr.forEach((item) => {
-		const { _id, images } = item;
-		result[_id] = images;
-	});
-
-	return result;
-}
-
-function transformContents(arr) {
-	const result = {};
-
-	arr.forEach((item) => {
-		const { _id, contents } = item;
-		// Parse the contents string into an array
-		result[_id] = contents;
-	});
-
-	return result;
 }
