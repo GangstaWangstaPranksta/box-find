d05b1f5c8ffb8469edd11a2880b66485dd5b6f5d
Jason <jason@jasonx.dev>
2025-01-16 19:47:28 -0500

    Update sveltekit adapter to bun

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from 'svelte-adapter-bun';
 import { optimizeImports } from 'carbon-preprocess-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
