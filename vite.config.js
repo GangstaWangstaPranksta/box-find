775c5f95034b7d571da3fb206555f65a42f668a3
Jason <jason@jasonx.dev>
2025-01-16 19:30:42 -0500

    Add carbon preprocessor

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { optimizeCss } from 'carbon-preprocess-svelte';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), optimizeCss()],
 	build: {
 		rollupOptions: {
-		  external: [
-			"sharp"
-		  ]
+			external: [
+				"sharp"
+			]
 		}
-	  }
+	}
 });
