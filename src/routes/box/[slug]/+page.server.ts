31d9e380b3f01754e18c85851ef450c86fc161d1
Jason <jason@jasonx.dev>
2025-01-22 19:51:48 -0500

    rewrite using mongoose drivers

--- src/routes/box/[slug]/+page.server.ts
+++ src/routes/box/[slug]/+page.server.ts
@@ -1,45 +1,22 @@
-import { MongoClient, ServerApiVersion } from 'mongodb';
-import type { PageLoad } from './$types';
+import type { boxData } from '$lib/types/types';
+import Box from '$lib/models/box';
+import connectDB from '$lib/db/connect';
+import type { PageServerLoad } from './$types';
 import dotenv from 'dotenv';
 dotenv.config();
 
-let uri;
-if (process.env.NODE_ENV !== 'production') {
-	uri = `mongodb+srv://${process.env.MONGO_URI}`;
-} else {
-	uri = `mongodb://${process.env.MONGO_URI}`;
-}
-
-const client = new MongoClient(uri, {
-	serverApi: {
-		version: ServerApiVersion.v1,
-		strict: true,
-		deprecationErrors: true
-	}
-});
-
-export const load: PageLoad = async ({ params }) => {
+export const load: PageServerLoad = async ({ params }) => {
 	const id = params.slug;
-	let contents, images, boxExist;
-	try {
-		// Connect the client to the server	(optional starting in v4.7)
-		await client.connect();
-		const collection = client.db('test-box-db').collection('boxes');
+	await connectDB();
 
-		let box = await collection.findOne({ _id: id }, { sort: { lastModified: -1 }, projection: {} });
-
-		boxExist = box != null;
-		contents = boxExist ? box.contents : '';
-		images = boxExist ? box.images : [];
-	} finally {
-		// Ensures that the client will close when you finish/error
-		if (client) await client.close();
-	}
+	// Use .lean() to get a plain JavaScript object
+	const box: boxData = await Box.findOne({ _id: id }).lean().exec();
+	const boxExist = box != null;
 
 	return {
-		box: id,
-		contents,
-		images,
-		boxExist
+		box: boxExist ? box._id : null,
+		contents: boxExist ? box.contents : '',
+		images: boxExist ? box.images : [],
+		boxExist: boxExist
 	};
 };
--- src/routes/page/[slug]/+page.server.ts
+++ src/routes/page/[slug]/+page.server.ts
@@ -1,25 +1,11 @@
-import { MongoClient, ServerApiVersion } from 'mongodb';
-import type { PageLoad } from './$types';
+import Box from '$lib/models/box';
+import connectDB from '$lib/db/connect';
+import type { PageServerLoad } from './$types';
 import dotenv from 'dotenv';
 import { redirect } from '@sveltejs/kit';
 dotenv.config();
 
-let uri;
-if (process.env.NODE_ENV !== 'production') {
-	uri = `mongodb+srv://${process.env.MONGO_URI}`;
-} else {
-	uri = `mongodb://${process.env.MONGO_URI}`;
-}
-
-const client = new MongoClient(uri, {
-	serverApi: {
-		version: ServerApiVersion.v1,
-		strict: true,
-		deprecationErrors: true
-	}
-});
-
-export const load: PageLoad = async ({ params }) => {
+export const load: PageServerLoad = async ({ params }) => {
 	//check if the page is a number, if not redirect to home
 	const regex = /^[0-9]+$/;
 	if (!regex.test(params.slug)) redirect(307, `/`);
@@ -27,34 +13,29 @@ export const load: PageLoad = async ({ params }) => {
 	if (page == 0) redirect(307, `/`);
 	let lastPage;
 	let contents;
-	try {
-		// Connect the client to the server	(optional starting in v4.7)
-		await client.connect();
-		const collection = client.db('test-box-db').collection('boxes');
 
-		//if current page is too high, set it to the last page
-		let count = await collection.countDocuments();
-		lastPage = Math.ceil(count / 10);
-		if (page > lastPage) {
-			page = lastPage;
-			redirect(307, `/page/${page}`);
-		}
+	await connectDB();
 
-		let contentsCursor = collection
-			.find({}, { sort: { lastModified: -1 }, projection: { contents: 1, images: 1 } })
-			.skip((page - 1) * 10)
-			.limit(10);
-
-		contents = await contentsCursor.toArray();
-		for (let i = 0; i < contents.length; i++) {
-			if (contents[i].images?.length > 5) {
-				contents[i].images = contents[i].images.slice(0, 5);
-			}
-		}
-	} finally {
-		// Ensures that the client will close when you finish/error
-		if (client) await client.close();
+	const count = await Box.countDocuments();
+	lastPage = Math.ceil(count / 10);
+	if (page > lastPage) {
+		page = lastPage;
+		redirect(307, `/page/${page}`);
 	}
+
+	const boxes = await Box.find({}, 'images contents')
+		.sort({ lastModified: -1 })
+		.skip((page - 1) * 10)
+		.limit(10)
+		.lean()
+		.exec();
+
+	contents = boxes.map((box) => {
+		if (box.images.length > 5) {
+			box.images = box.images.slice(0, 5);
+		}
+		return box;
+	});
 
 	return {
 		contents: contents,
