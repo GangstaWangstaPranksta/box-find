c814773f2c75915abe6f72e648856c381cabc170
Jason <jason@jasonx.dev>
2025-01-25 00:00:22 -0500

    use try catch

--- src/routes/api/renameBox/+server.ts
+++ src/routes/api/renameBox/+server.ts
@@ -13,19 +13,26 @@ export const PATCH: RequestHandler = async ({ request }) => {
 	if (id === editBoxName) {
 		return json({ error: 'New name cannot be the same as the old name' }, { status: 400 });
 	}
-	await connectDB();
+	try {
+		await connectDB();
 
-	const originalBox = await Box.findById(id);
-	if (!originalBox) {
-		return json({ error: 'Box not found' }, { status: 404 });
+		const originalBox = await Box.findById(id);
+		if (!originalBox) {
+			return json({ error: 'Box not found' }, { status: 404 });
+		}
+		const newBox = await Box.create({
+			_id: editBoxName,
+			contents: originalBox.contents,
+			images: originalBox.images,
+			lastModified: Date.now()
+		});
+		await Box.findByIdAndDelete({ _id: id });
+
+		return json({ newID: newBox._id }, { status: 202 });
+	} catch (e) {
+		return json(
+			{ error: 'Unexpected Server Error', details: (e as Error).message },
+			{ status: 500 }
+		);
 	}
-	const newBox = await Box.create({
-		_id: editBoxName,
-		contents: originalBox.contents,
-		images: originalBox.images,
-		lastModified: Date.now()
-	});
-	await Box.findByIdAndDelete({ _id: id });
-
-	return json({ newID: newBox._id }, { status: 202 });
 };
