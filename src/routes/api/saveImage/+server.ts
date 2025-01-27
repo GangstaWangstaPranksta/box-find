4ce6e999680e673c574d7e03516af93dcdcec204
Jason <jason@jasonx.dev>
2025-01-27 01:12:37 -0500

    use try catch

--- src/routes/api/saveImage/+server.ts
+++ src/routes/api/saveImage/+server.ts
@@ -12,26 +12,33 @@ export const PATCH: RequestHandler = async ({ request }) => {
 		return json({ error: 'Missing id or base64' }, { status: 400 });
 	}
 
-	const base64Data = base64.replace(/^data:image\/\w+;base64,/, '');
-	const imageBuffer = Buffer.from(base64Data, 'base64');
+	try {
+		const base64Data = base64.replace(/^data:image\/\w+;base64,/, '');
+		const imageBuffer = Buffer.from(base64Data, 'base64');
 
-	const compressedBuffer = await sharp(imageBuffer)
-		.withMetadata()
-		.resize(6500, 6500, { fit: 'inside' })
-		.jpeg({ quality: 75 })
-		.toBuffer();
+		const compressedBuffer = await sharp(imageBuffer)
+			.withMetadata()
+			.resize(6500, 6500, { fit: 'inside' })
+			.jpeg({ quality: 75 })
+			.toBuffer();
 
-	const compressedBase64 = `data:image/jpeg;base64,${compressedBuffer.toString('base64')}`;
+		const compressedBase64 = `data:image/jpeg;base64,${compressedBuffer.toString('base64')}`;
 
-	await connectDB();
-	const box = await Box.findById(id);
-	if (box) {
-		box.images.push(compressedBase64);
-		box.lastModified = Date.now();
-		await box.save();
-	} else {
-		return json({ error: 'Box not found' }, { status: 404 });
+		await connectDB();
+		const box = await Box.findById(id);
+		if (box) {
+			box.images.push(compressedBase64);
+			box.lastModified = Date.now();
+			await box.save();
+		} else {
+			return json({ error: 'Box not found' }, { status: 404 });
+		}
+
+		return json({ status: 'ok' });
+	} catch (e) {
+		return json(
+			{ error: 'Unexpected Server Error', details: (e as Error).message },
+			{ status: 500 }
+		);
 	}
-
-	return json({ status: 'ok' });
 };
